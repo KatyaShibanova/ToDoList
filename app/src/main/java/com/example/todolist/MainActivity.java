83b91a7be1e7d657e8cfd92a8c95dbe35e748f15
Katyusha <masiasha@mail.ru>
2020-03-05 12:49:15 +0300

    Главная страница приложения

---
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
