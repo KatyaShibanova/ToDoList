b7676e72b377a0534a80a6e6cd477e8940b689ac
Katyusha <masiasha@mail.ru>
2020-03-18 11:31:58 +0300

    создание базы данных (я и Софа)

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import android.view.View;
@@ -24,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
     public void new_task_click(View view) {
         Intent intent = new Intent(MainActivity.this, new_task.class);
         startActivity(intent);
+    }
+
+    public void database() {
+        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS tasks (id INTEGER, importance INTEGER, description TEXT, deadline TEXT)");
     }
 
 
